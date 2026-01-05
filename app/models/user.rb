e314a700c5cc980fc15e1dd8ab43baa27af104c9
rgyt <103154533+rg-yt@users.noreply.github.com>
2026-01-04 21:19:57 -0500

    Update assocations for user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable, :rememberable
   devise :database_authenticatable, :registerable, :validatable
+  after_create :create_profile
+  has_one :profile, dependent: :destroy
   has_many :posts
   has_many :comments
   has_many :likes
   has_many :followings
   has_many :followed_users, through: :followings, source: :followed_user
+
+
+  private
+
+  def create_profile
+    Profile.create(user_id: self.id, username: self.email.split("@")[0])
+  end
 end
