7ca26090e2ba48f9071b284992f7c88e34f58ae5
rgyt <103154533+rg-yt@users.noreply.github.com>
2026-01-05 14:59:51 -0500

    Add like validation to reflect database constraint

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likable, polymorphic: true
+
+  validates :user_id, uniqueness: { scope: [ :likable_id, :likable_type ], message: "has already liked this content" }
 end
