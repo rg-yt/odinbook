ad9656da682e969c7e6fca6eb8e81debc0770008
rgyt <103154533+rg-yt@users.noreply.github.com>
2026-01-06 22:34:28 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "profiles/show"
   get "posts/index"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -13,4 +14,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "posts#index"
+  resources :profiles, only: [ :show ]
 end
