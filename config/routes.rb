71d13f0df413fde44292e27518c3ccb1a53f98bb
rgyt <103154533+rg-yt@users.noreply.github.com>
2026-01-07 23:20:20 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "posts#index"
   resources :profiles, only: [ :show ]
+  resources :followings, only: [ :create ]
 end
