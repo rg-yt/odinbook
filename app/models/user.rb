6e8e73049d624a8c234faeed88c1992e69a1291e
rgyt <103154533+rg-yt@users.noreply.github.com>
2026-01-11 20:00:51 -0500

    Add google omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :recoverable, :rememberable
   devise :database_authenticatable, :registerable, :validatable
+  devise :omniauthable, omniauth_providers: [ :google_oauth2 ]
   after_create :create_profile
   has_one :profile, dependent: :destroy
   has_many :posts
@@ -11,6 +12,17 @@ class User < ApplicationRecord
   has_many :followed_users, through: :followings, source: :followed_user
 
 
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data["email"]).first
+    unless user
+        user = User.create(
+           email: data["email"],
+           password: Devise.friendly_token[0, 20]
+        )
+    end
+    user
+  end
   private
 
   def create_profile
