f8da9f27f80014e56e920908fb6bd1ab84285d20
rgyt <103154533+rg-yt@users.noreply.github.com>
2026-01-15 20:38:56 -0500

    Set up post creation with view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.post_feed(current_user)
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entitiy
+    end
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :title, :body, :user_id ])
+  end
 end
