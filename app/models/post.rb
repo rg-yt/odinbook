5d263daedc4d1aece1a54b10dfd5ae0055bb8a89
rgyt <103154533+rg-yt@users.noreply.github.com>
2026-01-19 00:27:35 -0500

    Update post feed to order by created

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes, as: :likable
 
-  scope :post_feed, ->(current_user) { Post.where(user_id: current_user.followed_user_ids + [ current_user.id ]).order(:created_at) }
+  scope :post_feed, ->(current_user) { Post.where(user_id: current_user.followed_user_ids + [ current_user.id ]).order(created_at: :desc) }
 end
